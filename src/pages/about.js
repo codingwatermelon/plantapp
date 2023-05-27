418f8719880f1b36135f0afdd29794b6e4c41fda
codingwatermelon <jtorres7797@gmail.com>
2023-05-27 14:03:04 -0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from 'react';
   
-const About = () => {
+const about = () => {
   return (
     <div
       style={{
@@ -15,4 +15,4 @@ const About = () => {
   );
 };
   
-export default About;
+export default about;
